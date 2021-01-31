db8d84905b405cad32c18a9344e08099d8dc292b
raulmanzas <raulmanzas@gmail.com>
2021-01-31 17:43:42 -0300

    adds FindGame method on GameBacklogService proto definition

--- src/main/kotlin/com/learning/endpoint/GameBacklogEndpoint.kt
+++ src/main/kotlin/com/learning/endpoint/GameBacklogEndpoint.kt
@@ -2,6 +2,8 @@ package com.learning.endpoint
 
 import com.learning.generated.grpc.AddToBacklogRequest
 import com.learning.generated.grpc.AddToBacklogResponse
+import com.learning.generated.grpc.FindGameRequest
+import com.learning.generated.grpc.FindGameResponse
 import com.learning.generated.grpc.GameBacklogServiceGrpc.GameBacklogServiceImplBase
 import io.grpc.stub.StreamObserver
 import javax.inject.Singleton
@@ -10,5 +12,9 @@ import javax.inject.Singleton
 class GameBacklogEndpoint : GameBacklogServiceImplBase() {
     override fun addToBacklog(request: AddToBacklogRequest?, responseObserver: StreamObserver<AddToBacklogResponse>?) {
         super.addToBacklog(request, responseObserver)
+    }
+
+    override fun findGame(request: FindGameRequest?, responseObserver: StreamObserver<FindGameResponse>?) {
+        super.findGame(request, responseObserver)
     }
 }
