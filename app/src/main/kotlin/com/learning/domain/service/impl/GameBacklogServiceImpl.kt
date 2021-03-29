c432d507d94b8f034406d4e9d383fa70c6823a2c
raulmanzas <raulmanzas@gmail.com>
2021-03-28 21:26:17 -0300

    adds logging calls to enpoint and services

--- app/src/main/kotlin/com/learning/domain/service/impl/GameBacklogServiceImpl.kt
+++ app/src/main/kotlin/com/learning/domain/service/impl/GameBacklogServiceImpl.kt
@@ -3,6 +3,8 @@ package com.learning.domain.service.impl
 import com.learning.domain.model.Game
 import com.learning.domain.persistence.GameRepository
 import com.learning.domain.service.GameBacklogService
+import com.learning.endpoint.GameBacklogEndpoint
+import org.slf4j.LoggerFactory
 import java.util.Optional
 import javax.inject.Singleton
 
@@ -11,8 +13,14 @@ internal class GameBacklogServiceImpl(
     private val gameRepository: GameRepository
 ): GameBacklogService {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(GameBacklogEndpoint::class.java)
+    }
+
     override fun addGameToBacklog(game: Game): Game {
-        return Game()
+        game.validate()
+        logger.info("game {} is valid and is going to be persisted", game.title)
+        return gameRepository.persist(game)
     }
 
     override fun findGameById(id: Long): Optional<Game> {
--- app/src/main/kotlin/com/learning/endpoint/GameBacklogEndpoint.kt
+++ app/src/main/kotlin/com/learning/endpoint/GameBacklogEndpoint.kt
@@ -8,6 +8,7 @@ import com.learning.generated.grpc.AddToBacklogResponse
 import com.learning.generated.grpc.FindGameRequest
 import com.learning.generated.grpc.FindGameResponse
 import com.learning.generated.grpc.GameBacklogServiceGrpcKt.GameBacklogServiceCoroutineImplBase
+import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 @Singleton
@@ -15,13 +16,19 @@ class GameBacklogEndpoint(
     private val gameBacklogService: GameBacklogService
 ): GameBacklogServiceCoroutineImplBase() {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(GameBacklogEndpoint::class.java)
+    }
+
     override suspend fun addToBacklog(request: AddToBacklogRequest): AddToBacklogResponse {
+        logger.info("received request to add game to backlog: {}", request)
         val newGame = request.asModel()
         val game = gameBacklogService.addGameToBacklog(newGame)
         return game.asGrpcMessage()
     }
 
     override suspend fun findGame(request: FindGameRequest): FindGameResponse {
+        logger.info("received request to find game: {}", request)
         return super.findGame(request)
     }
 }
